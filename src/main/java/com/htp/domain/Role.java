a9a56e34f74a4ab1ff4ac7b9fe3956626edada0a
kuzmichalex <58950628+kuzmichalex@users.noreply.github.com>
2020-06-04 18:40:23 +0300

    added methods equals, hashCode, toString

--- src/main/java/com/htp/domain/Role.java
+++ src/main/java/com/htp/domain/Role.java
@@ -3,40 +3,41 @@ package com.htp.domain;
 import java.util.Objects;
 
 public class Role {
-  private long id;
-  private String roleName;
+	private long id;
+	private String roleName;
 
-  public long getId() {
-    return id;
-  }
-  public void setId(long id) {
-    this.id = id;
-  }
+	public long getId() {
+		return id;
+	}
 
-  public String getRoleName() {
-    return roleName;
-  }
+	public void setId(long id) {
+		this.id = id;
+	}
 
-  public void setRoleName(String roleName) {
-    this.roleName = roleName;
-  }
+	public String getRoleName() {
+		return roleName;
+	}
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, roleName);
-  }
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null || getClass() != obj.getClass()) return false;
-    Role role = (Role) obj;
-    return Objects.equals(id, role.id) &&
-            Objects.equals(roleName, role.roleName);
-  }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, roleName);
+	}
 
-  @Override
-  public String toString() {
-    return "Role{" + "id=" + id + ", roleName='" + roleName + '\'' + '}';
-  }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null || getClass() != obj.getClass()) return false;
+		Role role = (Role) obj;
+		return Objects.equals(id, role.id) &&
+				Objects.equals(roleName, role.roleName);
+	}
+
+	@Override
+	public String toString() {
+		return "Role{" + "id=" + id + ", roleName='" + roleName + '\'' + '}';
+	}
 }
